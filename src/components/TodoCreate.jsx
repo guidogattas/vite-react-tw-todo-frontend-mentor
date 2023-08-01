54db98a6c4bd80552f230ee8aea88871bf711807
Guido Gattas <guido.gattas@gmail.com>
2023-08-01 17:50:43 -0300

    form controlado

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 const TodoCreate = ({ createTodo }) => {
 
+    const [title, setTitle] = useState("")
+
+    const handleSubmitAddTodo = (e) => {
+        e.preventDefault();
+        if (!title.trim()) {
+            return setTitle("")
+        }
+        else {
+            createTodo(title);
+            setTitle("");
+        }
+    }
+
+
     return (
-        <form className="flex items-center gap-4 px-4 py-4 mt-8 overflow-hidden bg-white rounded-md ">
+        <form onSubmit={handleSubmitAddTodo} className="flex items-center gap-4 px-4 py-4 mt-8 overflow-hidden bg-white rounded-md ">
             <span className="inline-block w-5 h-5 bg-gray-300 border-2 rounded-full"></span>
             <input
                 className="w-full text-gray-400 outline-none"
                 type="text"
                 placeholder="Create a new Todo"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
             />
         </form>
     )
