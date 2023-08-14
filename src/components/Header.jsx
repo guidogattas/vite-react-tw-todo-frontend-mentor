6ede515e7ce675696754ecebfce2da8a31af7507
Guido Gattas <guido.gattas@gmail.com>
2023-08-14 18:01:18 -0300

    todo animacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,18 +20,37 @@ const Header = () => {
     }
   }, [darkMode]);
 
+  const transitionLetter = "transition-transform duration-300 ease-in-out origin-top group-hover:scale-y-0";
+  const bottomTransitionLetter = "transition-transform duration-300 ease-in-out origin-bottom scale-y-0 group-hover:scale-y-100";
+
 
   return (
-    <header className="container px-4 pt-8 md:max-w-xl mx-auto font-semibold">
+    <header className="container px-4 pt-8 mx-auto font-semibold md:max-w-xl">
       <div className="flex justify-between">
-        <h1 className="uppercase text-white text-3xl tracking-[0.3em]">
-          Todo
-        </h1>
+        <div className="flex items-center">
+          <div className="relative p-4 text-white transition duration-500 ease-in-out bg-transparent border border-none rounded-full cursor-pointer md:p-8 group ">
+            <div className="relative flex items-center text-6xl uppercase">
+              <div className={`${transitionLetter}`} style={{ transitionDelay: "0ms" }}>T</div>
+              <div className={`${transitionLetter}`} style={{ transitionDelay: "75ms" }}>O</div>
+              <div className={`${transitionLetter}`} style={{ transitionDelay: "150ms" }}>D</div>
+              <div className={`${transitionLetter}`} style={{ transitionDelay: "225ms" }}>O</div>
+            </div>
+            <div className="absolute flex items-center text-6xl uppercase bottom-3">
+              <div className={`${bottomTransitionLetter}`} style={{ transitionDelay: "0ms" }}>T</div>
+              <div className={`${bottomTransitionLetter}`} style={{ transitionDelay: "75ms" }}>O</div>
+              <div className={`${bottomTransitionLetter}`} style={{ transitionDelay: "150ms" }}>D</div>
+              <div className={`${bottomTransitionLetter}`} style={{ transitionDelay: "225ms" }}>O</div>
+            </div>
+          </div>
+        </div>
+
+
+
         <button onClick={() => setDarkMode(!darkMode)}>
           {darkMode ? <IconSun /> : <IconMoon />}
         </button>
       </div>
-    </header>
+    </header >
   )
 }
 
